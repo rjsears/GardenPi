8245131be4b165838f1d419bf2e7f5062cca8ab7
rjsears <richardjsears@gmail.com>
2020-08-04 08:43:52 -0700

    fix route import

--- run.py
+++ run.py
@@ -8,17 +8,13 @@ run.py for use with neptune/GardenPi V1.0.0
 Starts flask dev server.
 """
 
-VERSION = "V1.0.0 (2020-07-31)"
+VERSION = "V1.0.0 (2020-08-04)"
 
 from system_logging import setup_logging
 from system_logging import read_logging_config
 import logging
+from gardenpi import app
 
-#Setup Module level logging here. Main logging config in system_logging.py
-
-
-#from gardenpi import app
-from routes import app
 
 if __name__ == '__main__':
     setup_logging()
